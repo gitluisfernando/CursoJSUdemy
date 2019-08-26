33b4112f685c9c3fe75ec37f4183ccda9c8fc528
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2019-08-26 19:13:27 -0300

    Estudos String e Template string

---
+++ Exercicios-JS/Fundamentos/stings.js
@@ -0,0 +1,7 @@
+const texto = "Luis Fernando Ribeiro"
+
+console.log(texto)
+console.log(texto.charAt(0)) //Letra na posição zero
+console.log("Texto ".concat(texto))
+console.log("Texto " + texto + "!")//Concatenação
+console.log(texto.replace("s", "z"))
