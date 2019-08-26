5073a735d30590e1222f426267e0f55a91a79381
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2019-08-26 19:34:45 -0300

    Estudo boolean

---
+++ Exercicios-JS/Fundamentos/booleanos.js
@@ -0,0 +1,8 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+let nome = ""
+console.log(nome || "Desconhecido")//Preencher por padrão como desconhecido caso o nome não tenha sido preenchido
