2b3108c7d243b332827129fac52eb1d5b0834ff5
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2019-08-27 06:24:19 -0300

    Aula 25 - Tipos em JS: Object

---
+++ Exercicios-JS/Fundamentos/objetos.js
@@ -0,0 +1,23 @@
+const objetoDinamico = {}
+objetoDinamico.nome = "Luis Fernando"
+objetoDinamico.preco = 5000.00
+
+console.log(objetoDinamico)
+
+const objeto = {nome:"Bianca", idade:24}
+console.log(objeto)
+
+const objetoCompleto = {
+    nome: "Alexandre",
+    idade: 30
+}
+console.log(objetoCompleto)
+
+const objetoComObjeto = {
+    nome: "Objetão",
+    precos: {
+        preco1: 1.00,
+        preco2: 2.00
+    }
+}
+console.log(objetoComObjeto)
