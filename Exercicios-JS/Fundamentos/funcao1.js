55f7d55564df596246cee16fe6269e8878cc2b83
luis.ribeiro <mail.luisfernando@gmail.com>
2019-08-27 12:45:28 -0300

    Aulas 27 a 29 - Básico de Funções

---
+++ Exercicios-JS/Fundamentos/funcao1.js
@@ -0,0 +1,13 @@
+//Função sem retorno
+function imprimirSoma(valor1, valor2){
+    console.log(valor1 + valor2)
+}
+
+imprimirSoma(2,3)
+imprimirSoma("Luis ", "Fernando")
+
+//Função com retorno
+function soma(a,b){
+    return a + b
+}
+console.log(soma(2,3))
