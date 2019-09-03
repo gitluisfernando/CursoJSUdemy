098362cdc644e4f29d2d6a64e28085275bb9c836
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2019-09-03 06:29:05 -0300

    Ajuste estrutura

---
+++ Exercicios-JS/2 - EstruturasDeControle/forIn.js
@@ -0,0 +1,14 @@
+const notas = [6.5, 7.5, 8.5, 9.5]
+
+for (i in notas) {
+    console.log(i, notas[i])
+}
+
+const pessoa = {
+    nome: "Luis",
+    idade: 29
+}
+
+for (atributos in pessoa) {
+    console.log(atributos, pessoa[atributos])
+}
