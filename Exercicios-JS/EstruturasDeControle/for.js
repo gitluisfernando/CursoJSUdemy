eaf8fa8f41677f710d57d6eeb508a0f91838e377
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2019-09-03 06:17:46 -0300

    Aula 61 - For

---
+++ Exercicios-JS/EstruturasDeControle/for.js
@@ -0,0 +1,8 @@
+for (let contador = 1; contador <= 10; contador++){
+    console.log(contador)
+}
+
+const notas = [9.5, 4.8, 9, 6.9]
+for (let i=0; i < notas.length; i++){
+    console.log(`Nota: ${notas[i]}`)
+}
